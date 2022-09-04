3f935519a60e39f1b0acd60a0a98fcfbdfa748a4
lance@home <lancehuang79@gmail.com>
2022-09-04 21:24:15 +0800

    use static method to make code readable

--- src/main/java/idv/lance/StrategyExample.java
+++ src/main/java/idv/lance/StrategyExample.java
@@ -10,8 +10,8 @@ public class StrategyExample {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         System.out.println(totalValue(numbers, i -> true));
-        System.out.println(totalValue(numbers, i -> i % 2 == 0));
-        System.out.println(totalValue2(numbers, i -> i % 2 != 0));
+        System.out.println(totalValue(numbers, Util::isEven));
+        System.out.println(totalValue2(numbers, Util::isOdd));
     }
 
     public static Integer totalValue(Collection<Integer> numbers, Predicate<Integer> selector) {
@@ -21,5 +21,14 @@ public class StrategyExample {
     public static Integer totalValue2(Collection<Integer> numbers, Predicate<Integer> selector) {
         return numbers.stream().filter(selector).mapToInt(i -> i).sum();
     }
+}
 
+class Util {
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    public static boolean isOdd(int number) {
+        return number % 2 != 0;
+    }
 }
