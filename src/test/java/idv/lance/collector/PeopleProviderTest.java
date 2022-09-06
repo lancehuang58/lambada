75afdd282abd87d1862ca47927da90acb3fb3b15
lance@office <lance@delta.office>
2022-09-06 13:35:47 +0800

    #age total assertion

--- src/test/java/idv/lance/collector/PeopleProviderTest.java
+++ src/test/java/idv/lance/collector/PeopleProviderTest.java
@@ -1,18 +1,23 @@
 package idv.lance.collector;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 class PeopleProviderTest {
 
+
+    //reduce takes the collection reduces to a single value...
+    //reduces convert a stream to something concrete.
+    //java has reduces in two forms, reduce and collect
     @Test
     void test_people_age_sum_reduce() {
         List<Person> people = PeopleProvider.get();
-
         //int ageTotal = people.stream().map(Person::getAge).reduce(0, (total, age) -> total + age);
         int ageTotal = people.stream().map(Person::getAge).reduce(0, Integer::sum);
-
-        System.out.println(ageTotal);
+        Assertions.assertEquals(212, ageTotal);
     }
+
+
 }
