7233a0cd0b0db772deae16b2d0412cb99bee1850
Lance Huang <lance.huang@deltaww.com>
2023-03-23 13:42:12 +0800

    palindrome /prime numbers

--- src/main/java/idv/lance/IsPalindromePrimeNumber.java
+++ src/main/java/idv/lance/IsPalindromePrimeNumber.java
@@ -1,55 +1,65 @@
 package idv.lance;
 
+import org.apache.commons.lang3.Range;
+
 import static java.util.stream.Collectors.*;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class IsPalindromePrimeNumber {
+    static Range<Integer> digit8Range = Range.between(10000000, 99999999);
+    static Range<Integer> digit6Range = Range.between(100000, 999999);
+    static Range<Integer> digit4Range = Range.between(1000, 9999);
 
-  public static void main(String[] args) {
-    long start = System.currentTimeMillis();
-    int N = 1000000000;
-    boolean[] isPrime = new boolean[N + 1];
-    isPrime[0] = isPrime[1] = true;
+    public static void main(String[] args) {
 
-    int[] Prime = new int[N];
-    int t = 0;
-    for (int i = 2; i < N; i++) {
-      if (!isPrime[i]) {
-        Prime[t++] = i;
-      }
+        long start = System.currentTimeMillis();
+        System.out.println("start = " + start);
+        int N = 1000000000;
+        boolean[] isPrime = new boolean[N];
+        isPrime[0] = isPrime[1] = true;
+        int[] prime = new int[N];
+        int t = 0;
+        for (int i = 2; i < N; i++) {
+            if (!isPrime[i]) {
+                prime[t++] = i;
+            }
 
-      for (int j = 0; j < t && Prime[j] * i < N; j++) {
-        isPrime[i * Prime[j]] = true;
-        if (i % Prime[j] == 0) break;
-      }
+            for (int j = 0; j < t && prime[j] * i < N; j++) {
+                int idx = i * prime[j];
+                isPrime[idx] = true;
+                if (i % prime[j] == 0) break;
+            }
+        }
+
+        List<Integer> primes = Arrays.stream(prime).filter(i -> i != 0).boxed().collect(toList());
+        System.out.println("primes size " + primes.size());
+        List<Integer> primesWithPalindrome = primes.stream().filter(IsPalindromePrimeNumber::isPalindrome).collect(toList());
+        System.out.println("primesWithPalindrome.size() = " + primesWithPalindrome.size());
+        System.out.println(primesWithPalindrome.stream().map(String::valueOf).collect(joining(",")));
+        long end = System.currentTimeMillis();
+        System.out.println("end = " + end);
+        System.out.println("time " + (end - start));
     }
 
-    List<Integer> primes = Arrays.stream(Prime).filter(i -> i != 0).boxed().collect(toList());
-    System.out.println("primes size " + primes.size());
 
-    List<Integer> primesWithPalindrome =
-        primes.stream().filter(IsPalindromePrimeNumber::isPalindrome).collect(toList());
-
-    System.out.println("primesWithPalindrome.size() = " + primesWithPalindrome.size());
-
-    System.out.println(primesWithPalindrome.stream().map(String::valueOf).collect(joining(",")));
-    long end = System.currentTimeMillis();
-    System.out.println("time " + (end - start));
-  }
-
-  public static boolean isPalindrome(int n) {
-    return reverse(n) == n;
-  }
-
-  public static int reverse(int n) {
-    int j = 0;
-    int temp = n;
-    while (temp != 0) {
-      j = j * 10 + temp % 10;
-      temp /= 10;
+    public static boolean isPalindrome(int n) {
+        if (digit8Range.contains(n) || digit6Range.contains(n) || digit4Range.contains(n)) {
+            return false;
+        }
+        return reverse(n) == n;
     }
-    return j;
-  }
+
+    public static int reverse(int n) {
+
+
+        int j = 0;
+        int temp = n;
+        while (temp != 0) {
+            j = j * 10 + temp % 10;
+            temp /= 10;
+        }
+        return j;
+    }
 }
