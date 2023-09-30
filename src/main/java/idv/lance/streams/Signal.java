3edabd2c9c16094c7073ce75e4aa3a42c6a19322
lance@dell <lancehuang79@gmail.com>
2023-09-30 23:02:59 +0800

    how to use optional for null and optional isPresent

--- src/main/java/idv/lance/streams/Signal.java
+++ src/main/java/idv/lance/streams/Signal.java
@@ -4,24 +4,8 @@ import java.util.Optional;
 import java.util.Random;
 import java.util.stream.Stream;
 
-import static java.lang.String.format;
 
-public class Signal {
-    private final String msg;
-
-    public Signal(String msg) {
-        this.msg = msg;
-    }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    @Override
-    public String toString() {
-        return format("Signal(%s)", msg);
-    }
-
+public record Signal(String msg) {
     static Random random = new Random(47);
 
     public static Signal morse() {
